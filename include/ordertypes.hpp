013cdbb80b3122579dd10334821d19df6f4a3d13
Hnodomar <bluesilverg22@gmail.com>
2021-09-05 21:23:48 +0100

    pod ordertype structs for cheap C-style casting

---
+++ include/ordertypes.hpp
@@ -0,0 +1,29 @@
+#ifndef ORDER_TYPES_HPP
+#define ORDER_TYPES_HPP
+
+#include <cstdint>
+
+// POD structs for cheap C-style casts
+// standard prohibits compiler reordering of struct memory layout
+// for the sake of C compatibility
+namespace tradeorder {
+struct AddOrder {
+    uint64_t order_id;
+    uint8_t side;
+    uint64_t price;
+    uint64_t ticker;
+    uint16_t quantity;
+    uint8_t username[20];
+};
+
+struct ModifyOrder {
+    uint64_t order_id;
+    uint16_t quantity;
+    uint8_t addOrRemove;
+};
+
+struct CancelOrder {
+    uint64_t order_id;
+};  
+}
+#endif
