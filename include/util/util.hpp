4d9e090211053276e6a5ffe48df771e3d30da519
Hnodomar <bluesilverg22@gmail.com>
2021-09-07 13:08:13 +0100

    unix timestamping utility (epoch: ns since midnight)

---
+++ include/util/util.hpp
@@ -0,0 +1,22 @@
+#ifndef UTIL_HPP
+#define UTIL_HPP
+
+#include <chrono>
+#include <cstdint>
+
+// misc utility functions
+
+int64_t getUnixTimestamp() {
+    using time = std::chrono::system_clock;
+    auto now = time::now();
+    int64_t tnow = time::to_time_t(now);
+    tm *date = std::localtime(&tnow);
+    date->tm_hour = 0;
+    date->tm_min = 0;
+    date->tm_sec = 0;
+    auto midnight = time::from_time_t(std::mktime(date));
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(now - midnight).count();
+}
+
+
+#endif
