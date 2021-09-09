1e1a2c37926cdec6a2bbe6899a74fea30417f04c
Hnodomar <bluesilverg22@gmail.com>
2021-09-09 17:20:06 +0100

    rpc job class

--- include/server/rpc/rpcjob.hpp
+++ include/server/rpc/rpcjob.hpp
@@ -5,18 +5,68 @@
 
 class RPCJob {
 public:
-    enum class AsyncOpType {
+    enum class AsyncOperationType {
         INVALID,
         QUEUED_REQUEST,
         READ,
         WRITE,
         FINISH
     }; 
-    RPCJob() 
+    RPCJob() :
+        curr_async_ops_(0),
+        read_in_progress_(false),
+        write_in_progress_(false),
+        on_done_called_(false)
     {}
     virtual ~RPCJob() {}
+    void asyncOperationStarted(AsyncOperationType op) {
+        ++curr_async_ops_;
+        switch(op) {
+            case AsyncOperationType::READ:
+                read_in_progress_ = true;
+                break;
+            case AsyncOperationType::WRITE:
+                write_in_progress_ = false;
+                break;
+            default:
+                break;
+        }
+    }
+    bool asyncOperationFinished(AsyncOperationType op) {
+        --curr_async_ops_;
+        switch(op) {
+            case AsyncOperationType::READ:
+                read_in_progress_ = false;
+                break;
+            case AsyncOperationType::WRITE:
+                write_in_progress_ = false;
+                break;
+            default:
+                break;
+        }
+        if (curr_async_ops_ == 0 && on_done_called_) {
+            done();
+            return false;
+        }
+        return true;
+    }
+    bool isAsyncReadInProgress() const {
+        return read_in_progress_;
+    }
+    bool isAsyncWriteInProgress() const {
+        return write_in_progress_;
+    }
+    void onDone(bool ) {
+        on_done_called_ = true;
+        if (curr_async_ops_ == 0)
+            done();
+    }
+    virtual void done() = 0;
 private:
-
+    int32_t curr_async_ops_;
+    bool read_in_progress_;
+    bool write_in_progress_;
+    bool on_done_called_;
 };
 
 
