58682ed01e9f14006d0793dee212c0dc1e693ee7
Hnodomar <bluesilverg22@gmail.com>
2021-09-10 20:40:38 +0100

    tradeserver update for rpc and rpc callback parallel processing

--- include/server/tradeserver.hpp
+++ include/server/tradeserver.hpp
@@ -5,27 +5,56 @@
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
-
+#include <list>
+#include <mutex>
 #include <grpcpp/grpcpp.h>
+#include <thread>
 
 #include "logger.hpp"
-#include "serverconnection.hpp"
 #include "ordermanager.hpp"
+#include "bidirstreamrpcjob.hpp"
+#include "unaryrpcjob.hpp"
+#include "orderentry.grpc.pb.h"
+#include "jobhandlers.hpp"
+#include "rpcprocessor.hpp"
+
+using ServiceType = orderentry::OrderEntryService::AsyncService;
+using OERequestType = orderentry::OrderEntryRequest;
+using OEResponseType = orderentry::OrderEntryResponse;
 
 namespace server {
-using tcp = boost::asio::ip::tcp;
 class TradeServer {
 public:
     TradeServer(char* port, const std::string& filename);
     ~TradeServer();
 private:
     void handleRemoteProcedureCalls();
+    struct OrderEntryResponder {
+        std::function<bool(OEResponseType*)> send_resp;
+        grpc::ServerContext* server_context;
+    };
     void createOrderEntryRPC();
+    void makeOrderEntryRPC();
+    static void setOrderEntryContext(
+        ServiceType* service, 
+        RPCJob* job, 
+        grpc::ServerContext* serv_context, 
+        std::function<bool(OEResponseType*)> response
+    );
+    static void orderEntryDone(ServiceType* service, RPCJob* job, bool);
+    static void orderEntryProcessor(RPCJob* job, const OERequestType* order_entry);
+    void makeMarketDataRPC();
+
     logging::Logger logger_;
+    std::mutex taglist_mutex_;
+    std::condition_variable condv_;
     tradeorder::OrderManager ordermanager_;
     std::unique_ptr<grpc::Server> trade_server_;
     std::unique_ptr<grpc::ServerCompletionQueue> cq_;
-    orderentry::OrderEntry::AsyncService order_entry_service_;
+    orderentry::OrderEntryService::AsyncService order_entry_service_;
+    std::list<RPC::CallbackTag> taglist_;
+    RPC::RPCProcessor rpc_processor_;
+    static std::unordered_map<RPCJob*, OrderEntryResponder> entry_order_responders_;
 };
 }
 
--- src/server/tradeserver.cpp
+++ src/server/tradeserver.cpp
@@ -3,7 +3,7 @@
 using namespace server;
 
 TradeServer::TradeServer(char* port, const std::string& outputfile="") 
-: logger_(outputfile) {
+: logger_(outputfile), rpc_processor_(taglist_, taglist_mutex_, condv_) {
     std::string server_address("0.0.0.0:" + std::string(port));
     grpc::ServerBuilder builder;
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
@@ -13,8 +13,48 @@ TradeServer::TradeServer(char* port, const std::string& outputfile="")
     logger_.write("Server listening on " + server_address);
 }
 
-void TradeServer::handleRemoteProcedureCalls() {
+void TradeServer::makeMarketDataRPC() {
 
+}
+
+void TradeServer::makeOrderEntryRPC() {
+    BiDirectionalStreamHandler<ServiceType, OERequestType, OEResponseType> job_handlers;
+    job_handlers.rpcJobContextHandler = &setOrderEntryContext;
+    job_handlers.rpcJobDoneHandler = &orderEntryDone;
+    job_handlers.createRPCJobHandler = std::bind(&TradeServer::makeOrderEntryRPC, this);
+    job_handlers.queueReqHandler = &orderentry::OrderEntryService::AsyncService::RequestOrderEntry;
+    job_handlers.processRequestHandler = &orderEntryProcessor;
+    new BiDirStreamRPCJob<ServiceType, OERequestType, OEResponseType>(&order_entry_service_, cq_.get(), job_handlers);
+}
+
+void TradeServer::setOrderEntryContext(ServiceType* service, RPCJob* job, 
+grpc::ServerContext* serv_context, std::function<bool(OEResponseType*)> send_response) {
+    OrderEntryResponder responder;
+    responder.send_resp = send_response;
+    responder.server_context = serv_context;
+    entry_order_responders_.emplace(job, responder);
+}
+
+void TradeServer::orderEntryDone(ServiceType* service, RPCJob* job, bool) {
+    entry_order_responders_.erase(job);
+    delete job;
+}
+
+void TradeServer::orderEntryProcessor(RPCJob* job, const OERequestType* order_entry) {
+
+}
+
+void TradeServer::handleRemoteProcedureCalls() {
+    makeOrderEntryRPC();
+    std::thread rpcprocessing(&rpc_processor_);
+    RPC::CallbackTag cb_tag;
+    for (;;) { // main grpc tag loop
+        GPR_ASSERT(cq_->Next((void**)&cb_tag.callback_fn, &cb_tag.ok));
+        taglist_mutex_.lock();
+        taglist_.push_back(cb_tag);
+        taglist_mutex_.unlock();
+        condv_.notify_one();
+    }
 }
 
 void TradeServer::createOrderEntryRPC() {
