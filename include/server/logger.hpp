9e8560c1a996fcc4ce8070b332f16505ca056d11
Hnodomar <bluesilverg22@gmail.com>
2021-09-28 16:39:39 +0100

    upgraded logging library

--- include/server/logger.hpp
+++ include/server/logger.hpp
@@ -7,7 +7,10 @@
 #include <memory>
 #include <atomic>
 #include <vector>
+#include <tuple>
 #include <thread>
+#include <string_view>
+#include <type_traits>
 #include <mutex>
 
 namespace logging {
@@ -18,17 +21,53 @@ enum class LogType {
     Error
 };
 
+inline std::ostream& operator<<(std::ostream& output, LogType type) {
+    switch(type) {
+        case LogType::Info: return output << "INFO";
+        case LogType::Debug: return output << "DEBUG";
+        case LogType::Warning: return output << "WARNING";
+        case LogType::Error: return output << "ERROR";
+    }
+}
+
 class LogEntry {
 public:
-    template<typename ...Args> LogEntry(Args&& ...Args) {
-
+    template<typename ...Args> LogEntry(Args&& ...args) {
+        formatter_ = packLogEntry(&log_entry_, std::forward<Args>(args)...);
     }
-    ~LogEntry() {
-
-    }
+    ~LogEntry() {formatter_(nullptr, &log_entry_);}
+    void formatLogEntry(std::ostream& output) {formatter_(&output, &log_entry_);}
 private:
+    static void format(std::ostream& output) {output << "\n";}
+    template <typename T, typename... Args>
+    static void format(std::ostream &o, T &&t, Args &&... args) {
+        o << t << " ";
+        format(o, std::forward<Args>(args)...);
+    }
+    template<typename Tuple, std::size_t ...index>
+    static void callFormatWithTupleValues(std::ostream& output, Tuple&& args, std::index_sequence<index...>) {
+        format(output, std::get<index>(std::forward<Tuple>(args))...);
+    }
+    template<typename ...Args>
+    static void makeTupleAndSequenceIt(std::ostream* output, void* log_entry) {
+        using Tuple = std::tuple<Args...>;
+        Tuple& args = *reinterpret_cast<Tuple*>(log_entry);
+        if (output)
+            callFormatWithTupleValues(*output, args, std::index_sequence_for<Args...>{});
+        else 
+            args.~Tuple();
+    }
+    template<typename T, typename ...Args>
+    static decltype(auto) packLogEntry(T* entry_storage, Args&& ...args) {
+        using Tuple = std::tuple<typename std::decay<Args>::type...>;
+        static_assert(alignof(Tuple) <= alignof(T));
+        static_assert(sizeof(Tuple) <= sizeof(T));
+        new(entry_storage) Tuple(std::forward<Args>(args)...);
+        return &makeTupleAndSequenceIt<typename std::decay<Args>::type...>;
+    }
     using Formatter = void(*)(std::ostream*, void*);
     Formatter formatter_;
+    std::aligned_storage<56, 8>::type log_entry_;
 };
 template<typename DataType> 
 class Queue {
@@ -68,31 +107,32 @@ public:
     }
 private:
     const std::size_t size_;
-    const DataType* buffer_;
+    DataType* const buffer_;
     std::atomic<std::size_t> head_, tail_;
 };
-// ideally: [loglevel] [datetime] [threadname:threadid] [msg] [module]
+// Ideally: [loglevel] [datetime] [threadname:threadid] [msg] [module]
 class Logger {
 public:
     template<typename ...Args>
-    static void LOG(const char* formatting, Args&& ...args) {
-        getQueue().emplace(formatting, std::forward<Args>(args)...);
+    static void Log(LogType type, Args&& ...args) {
+        getQueue().emplace(type, std::forward<Args>(args)...);
     }
-private:
-    Logger(const std::string& filename = "")
+    Logger(const std::string_view filename = "")
         : queue_size_(1024)
         , num_queues_(1)
-        , use_std_out_(true)
         , active_(true)
+        , use_std_out_(true)
     {
         if (filename != "")
-            output_ = std::make_unique<std::ofstream>(filename);
+            output_ = std::make_unique<std::ofstream>(std::string(filename));
         thread_ = std::thread([this]{writeFromQueue();});
     }
+
     ~Logger() {
         active_ = false;
         thread_.join();
     }
+private:
     void writeFromQueue() {
         while (active_ || num_queues_) {
             std::lock_guard<std::mutex> lock(getInstance().logger_mutex_);
@@ -100,10 +140,10 @@ private:
                 auto& queue = *itr;
                 while (queue->front()) {
                     if (output_) {
-                        queue->front()->Format(*(output_.get());
+                        queue->front()->formatLogEntry(*(output_.get()));
                     }
                     if (use_std_out_) {
-                        queue->front()->Format(std::cout);
+                        queue->front()->formatLogEntry(std::cout);
                     }
                     queue->pop();
                 }
@@ -120,10 +160,10 @@ private:
         static Logger instance;
         return instance;
     }
-    static Queue<std::string>& getQueue() {
-        static thread_local std::shared_ptr<Queue<std::string>> queue;
+    static Queue<LogEntry>& getQueue() {
+        static thread_local std::shared_ptr<Queue<LogEntry>> queue;
         if (queue == nullptr) {
-            queue = std::make_shared<Queue<std::string>>();
+            queue = std::make_shared<Queue<LogEntry>>(getInstance().queue_size_);
             std::lock_guard<std::mutex> lock(getInstance().logger_mutex_);
             getInstance().queues_.push_back(queue);
         }
@@ -132,12 +172,11 @@ private:
     std::mutex logger_mutex_;
     std::size_t queue_size_;
     std::size_t num_queues_;
-    std::vector<std::shared_ptr<Queue<std::string>>> queues_;
+    std::vector<std::shared_ptr<Queue<LogEntry>>> queues_;
     std::unique_ptr<std::ostream> output_;
     std::thread thread_;
     bool active_;
     bool use_std_out_;
-
 };
 }
 
